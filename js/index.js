ed755440322e32d0614853cb186b56e7cf5bbaec
Kewin Barboza <kbarboza@lockey.net.ve>
2020-12-01 11:34:58 -0400

    modulo de productos listo

--- js/index.js
+++ js/index.js
@@ -1,37 +1,29 @@
-const nameClient = document.getElementById('namePeople');
-const btnSaveClient = document.getElementById('btnSavePeople');
-const listProducts = document.getElementById('listProducts');
-const listClient = document.getElementById('list-client');
+const nameClient 		= document.getElementById('namePeople');
+const nameProduct 		= document.getElementById('name-product')
+const priceProduct 		= document.getElementById('price-product')
+const btnSaveClient 	= document.getElementById('btnSavePeople');
+const btnSaveProduct 	= document.getElementById('save-product');
+const btnModalClientes 	= document.getElementById('modal-clientes');
+const btnModalProductos = document.getElementById('modal-productos')
+const listProducts 		= document.getElementById('listProducts');
+const listClient 		= document.getElementById('list-client');
+const listProductsNew 	= document.getElementById('list-product');
 
 // agregar producto a clientes
 let addProducts = [];
 
-// lista de personas
-let clients = [
-	{
-		id: 1,
-		name: 'monica',
-		products: [
-			{ id: 1, name: 'galleta', price: 200, state: true },
-			{ id: 2, name: 'pan', price: 200, state: true },
-		],
-	},
-	{
-		id: 2,
-		name: 'erick',
-		products: [
-			{ id: 1, name: 'pan', price: 200, state: true },
-			{ id: 2, name: 'torta', price: 300, state: true },
-		],
-	},
-];
+// lista de personas 
+let clients = [];
 
 // lista de productos
-const products = [
-	{ id: 1, name: 'galleta', price: 200, state: false },
-	{ id: 2, name: 'pan', price: 200, state: false },
-	{ id: 3, name: 'torta', price: 300, state: false },
-];
+let products = [];
+
+// crear id unico para productos y clientes
+var idCounter = 0;
+function uniqueId(prefix) {
+    var id = ++idCounter + '';
+    return prefix ? prefix + id : id;
+};
 
 // agregar producto a los clientes
 function addProductPeople() {
@@ -54,18 +46,28 @@ function addProductPeople() {
 	});
 }
 
-// accion para agregar personas
-btnSaveClient.addEventListener('click', saveClient);
-
-// agregar parsonas
+// agregar personás
 function saveClient(e) {
 	e.preventDefault();
 	listClient.innerHTML = '';
-	const addClient = { id: 3, name: nameClient.value, products: addProducts };
+	const addClient = { id: uniqueId(), name: nameClient.value, products: addProducts };
 	clients = [...clients, addClient];
 	addProducts = [];
 	listClients();
 	limpiarModal('addPeople');
+	console.log(clients)
+}
+
+// guardar productos
+function saveProduct(e) {
+	e.preventDefault()
+	listProductsNew.innerHTML = ''
+	const addProduct = {id: uniqueId(), name:nameProduct.value, price: parseInt(priceProduct.value), state: false}
+	products.push(addProduct)
+	nameProduct.value = ''
+	priceProduct.value = ''
+	listProductNew();
+	deleteProduct();
 }
 
 function limpiarModal(nameModal) {
@@ -112,8 +114,74 @@ const listClients = () => {
 	listClient.appendChild(fragment);
 };
 
-listClients();
-listProduct();
-addProductPeople();
+// listar productos nuevos
+const listProductNew = () =>{
+	const templete = document.getElementById('templete-list-product').content
+	const fragment = document.createDocumentFragment()
 
-// console.log($('#productModal').modal());
+	products.map(product => {
+		templete.querySelector('#item-products p').textContent = product.name
+		templete.querySelector('#item-products span').textContent = product.price
+		templete.querySelector('button').setAttribute('id', product.id)
+
+		const clone = templete.cloneNode(true)
+		fragment.appendChild(clone)
+	})
+
+	listProductsNew.appendChild(fragment)
+}
+
+// eliminar producto
+const deleteProduct = () => {
+	const btnDeleteProduct = document.querySelectorAll('#item-products button')
+	
+	btnDeleteProduct.forEach(btnDelete => {
+		btnDelete.addEventListener('click', () =>{
+			listProductsNew.innerHTML = ''
+			const product = products.filter(product => product.id != btnDelete.id)
+			products = [...product]
+			listProductNew()
+		})
+	})
+}
+
+// abrir modal de clientes
+btnModalClientes.addEventListener('click', () =>{
+	// cargar lista de productos 
+	listProducts.innerHTML = ''
+	listProduct()
+	// agregar productos a los clientes
+	addProductPeople();
+	// acción para agregar personas
+	btnSaveClient.addEventListener('click', saveClient);
+})
+
+// abrir modal productos
+btnModalProductos.addEventListener('click', () =>{
+	listProductsNew.innerHTML = ''
+	// listar productos
+	listProductNew();
+	// acción para guardar productos
+	btnSaveProduct.addEventListener('click', saveProduct)
+})
+
+
+listClients();
+
+
+
+
+/*
+	-eliminar clientes
+	-editar clientes
+	-buscar clientes
+	-guardar en el localStorage
+	-acomodar precios
+	-modificar diseño
+	-refactorizar
+*/
+
+
+
+
+
