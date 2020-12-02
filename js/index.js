0fc170f55b1110f9f0701e0101ffa3c30532c5ac
Kewin Barboza <kbarboza@lockey.net.ve>
2020-12-02 12:14:36 -0400

    precios y funcion de eliminar por revisar

--- js/index.js
+++ js/index.js
@@ -1,22 +1,44 @@
-const nameClient 		= document.getElementById('namePeople');
-const nameProduct 		= document.getElementById('name-product')
-const priceProduct 		= document.getElementById('price-product')
-const btnSaveClient 	= document.getElementById('btnSavePeople');
-const btnSaveProduct 	= document.getElementById('save-product');
-const btnModalClientes 	= document.getElementById('modal-clientes');
-const btnModalProductos = document.getElementById('modal-productos')
-const listProducts 		= document.getElementById('listProducts');
-const listClient 		= document.getElementById('list-client');
-const listProductsNew 	= document.getElementById('list-product');
+const nameClient 			= document.getElementById('namePeople');
+const nameProduct 			= document.getElementById('name-product')
+const priceProduct 			= document.getElementById('price-product')
+const btnSaveClient 		= document.getElementById('btnSavePeople');
+const btnSaveProduct 		= document.getElementById('save-product');
+const btnModalClientes 		= document.getElementById('modal-clientes');
+const btnModalProductos 	= document.getElementById('modal-productos');
+const btnModalDeleteClient 	= document.getElementById('btn-delete-client')
+const listProducts 			= document.getElementById('listProducts');
+const listClient 			= document.getElementById('list-client');
+const listProductsNew 		= document.getElementById('list-product');
 
 // agregar producto a clientes
 let addProducts = [];
 
 // lista de personas 
-let clients = [];
+let clients = [
+	// {
+	// 	id: 1,
+	// 	name: 'monica',
+	// 	products: [
+	// 		{ id: 1, name: 'galleta', price: 200, state: true },
+	// 		{ id: 2, name: 'pan', price: 200, state: true },
+	// 	],
+	// },
+	// {
+	// 	id: 2,
+	// 	name: 'erick',
+	// 	products: [
+	// 		{ id: 1, name: 'pan', price: 200, state: true },
+	// 		{ id: 2, name: 'torta', price: 300, state: true },
+	// 	],
+	// },
+];
 
 // lista de productos
-let products = [];
+let products = [
+	// { id: 1, name: 'galleta', price: 200, state: false },
+	// { id: 2, name: 'pan', price: 200, state: false },
+	// { id: 3, name: 'torta', price: 300, state: false },
+];
 
 // crear id unico para productos y clientes
 var idCounter = 0;
@@ -41,6 +63,7 @@ function addProductPeople() {
 			} else {
 				const a = addProducts.filter((item) => item.id !== parseInt(check.id));
 				addProducts = [...a];
+				console.log(addProducts)
 			}
 		});
 	});
@@ -55,6 +78,28 @@ function saveClient(e) {
 	addProducts = [];
 	listClients();
 	limpiarModal('addPeople');
+	selectDeleteClient()
+}
+
+// eliminar clientes
+function selectDeleteClient(){
+	const btnDeleteClient = document.querySelectorAll('#action .btn-delete')
+	btnDeleteClient.forEach(btn => {
+		btn.addEventListener('click', () =>{
+			deleteClient(btn.id)
+		})
+	})
+} 
+
+function deleteClient(id){
+	console.log(id)
+	btnModalDeleteClient.addEventListener('click', () => {
+		const d = clients.filter(client => client.id !== id )
+		clients = [...d]
+		listClient.innerHTML = '';
+		listClients();
+	})
+
 	console.log(clients)
 }
 
@@ -62,7 +107,7 @@ function saveClient(e) {
 function saveProduct(e) {
 	e.preventDefault()
 	listProductsNew.innerHTML = ''
-	const addProduct = {id: uniqueId(), name:nameProduct.value, price: parseInt(priceProduct.value), state: false}
+	const addProduct = {id: parseInt(uniqueId()), name:nameProduct.value, price: parseInt(priceProduct.value), state: false}
 	products.push(addProduct)
 	nameProduct.value = ''
 	priceProduct.value = ''
@@ -98,15 +143,23 @@ const listClients = () => {
 	const templete = document.getElementById('templete-client').content;
 	const fragment = document.createDocumentFragment();
 
-	let total = 0;
 	for (const client of clients) {
 		templete.getElementById('name').textContent = client.name;
+		templete.querySelector('.btn-delete').setAttribute('id', client.id)
+		const products = templete.getElementById('products')
+		const price = templete.getElementById('price')
+		
+		products.innerHTML = ''
+		price.innerHTML = ''
+		let total = 0;
+
 		client.products.map((product) => {
-			total = product.price + product.price;
-			templete.getElementById('products').textContent = product.name;
-			console.log(total);
+			total += product.price
+			console.log(total) 
+			products.innerHTML += `<span class="badge badge-light mx-1"> ${product.name} - ${product.price} Bs  </span>`;
+			templete.getElementById('price').textContent = `${total} Bs`
 		});
-		templete.getElementById('price').textContent = total;
+
 		const clone = templete.cloneNode(true);
 		fragment.appendChild(clone);
 	}
@@ -138,7 +191,7 @@ const deleteProduct = () => {
 	btnDeleteProduct.forEach(btnDelete => {
 		btnDelete.addEventListener('click', () =>{
 			listProductsNew.innerHTML = ''
-			const product = products.filter(product => product.id != btnDelete.id)
+			const product = products.filter(product => product.id != parseInt(btnDelete.id))
 			products = [...product]
 			listProductNew()
 		})
@@ -176,7 +229,6 @@ listClients();
 	-editar clientes
 	-buscar clientes
 	-guardar en el localStorage
-	-acomodar precios
 	-modificar diseño
 	-refactorizar
 */
